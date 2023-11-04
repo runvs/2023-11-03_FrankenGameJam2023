a6cd9e9edc121f2a0c7bf3e30658692e4f63e34d
Simon Weis <laguna.ws@gmail.com>
2023-11-04 16:10:53 +0100

    Seemingly fixed something?

--- impl/gamelib/monkey/ai/ai_component_impl.cpp
+++ impl/gamelib/monkey/ai/ai_component_impl.cpp
@@ -2,11 +2,10 @@
 #include "math_helper.hpp"
 #include <iostream>
 
-float AiComponentImpl::getRotationAngle() {
-    return rotationAngle;
-}
+float AiComponentImpl::getRotationAngle() { return rotationAngle; }
 
-void AiComponentImpl::update(AiTargetInterface& target, const float elapsed) {
+void AiComponentImpl::update(AiTargetInterface& target, float const elapsed)
+{
     // TODO: if player enters range, do something silly
     auto const movementSpeed = 100;
     auto const rotationSpeed = 10000;
@@ -14,31 +13,31 @@ void AiComponentImpl::update(AiTargetInterface& target, const float elapsed) {
 
     auto const diff = playerPos - target.getPosition();
     auto const distanceToPlayer = jt::MathHelper::distanceBetween(playerPos, target.getPosition());
-    if (distanceToPlayer > chaseDistance)
-    {
+    if (distanceToPlayer > chaseDistance) {
         return;
     }
 
-    auto const targetRotation = jt::MathHelper::angleOf(playerPos - target.getPosition());
+    auto const targetRotation = jt::MathHelper::angleOf(diff);
 
-    //auto const angularDifference = targetRotation - rotationAngle;
-    //if (std::abs(angularDifference) > 1) {
-    //    if (angularDifference > 0) {
-    //        rotationAngle += rotationSpeed * elapsed;
-    //    } else {
-    //        rotationAngle -= rotationSpeed * elapsed;
-    //    };
-    //}
+    // auto const angularDifference = targetRotation - rotationAngle;
+    // if (std::abs(angularDifference) > 1) {
+    //     if (angularDifference > 0) {
+    //         rotationAngle += rotationSpeed * elapsed;
+    //     } else {
+    //         rotationAngle -= rotationSpeed * elapsed;
+    //     };
+    // }
 
     rotationAngle = targetRotation;
 
-    auto const force= jt::MathHelper::rotateBy(jt::Vector2f { 0.0f, -movementSpeed }, rotationAngle);
+    auto const force
+        = jt::MathHelper::rotateBy(jt::Vector2f { movementSpeed, 0.0f }, -rotationAngle);
     target.addForceToCenter(force);
 
-    std::cout << "monke pos: " << target.getPosition() << " player pos: " << playerPos << " diff: " << diff << " current: " << rotationAngle << " target: " << targetRotation
-              << std::endl;
+    //    std::cout << "monke pos: " << target.getPosition() << " player pos: " << playerPos
+    //              << " diff: " << diff << " current: " << rotationAngle << " target: " <<
+    //              targetRotation
+    //              << std::endl;
 }
 
-void AiComponentImpl::updatePlayerPosition(jt::Vector2f pos) {
-    playerPos = pos;
-}
+void AiComponentImpl::updatePlayerPosition(jt::Vector2f pos) { playerPos = pos; }
